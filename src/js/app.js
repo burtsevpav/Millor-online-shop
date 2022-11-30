7a6cbc74614e89b05f18a33ceeded03f49a9ac74
burtsevpav <pasha.burtsev.2015@mail.ru>
2022-11-30 11:33:39 +0300

    Создали страницу карточки с чаем product-tea.html, создали стили

--- src/js/app.js
+++ src/js/app.js
@@ -83,7 +83,7 @@ flsForms.formFieldsInit({ viewPass: false });
 flsForms.formSubmit();
 
 /* Модуль формы "колличество" */
-// flsForms.formQuantity();
+flsForms.formQuantity();
 
 /* Модуль звездного рейтинга */
 // flsForms.formRating();
